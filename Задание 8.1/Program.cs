a45ea658f1b5187d2bee838ed025f93613b79ac8
Anna <anna.vm@mail.ru>
2021-11-26 09:08:45 +0300

    Добавьте файлы проекта.

---
+++ Задание 8.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] allfiles = Directory.GetFiles("F:/Работа","*.*" , SearchOption.AllDirectories);
+            foreach (string filename in allfiles)
+            {
+                Console.WriteLine(filename);
+            }
+
+            string[] allfolders = Directory.GetDirectories("F:/Работа", "*.*", SearchOption.AllDirectories);
+            foreach (string folder in allfolders)
+            {
+                Console.WriteLine(folder);
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
